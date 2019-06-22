acf546002ac2cf1b2fdffe274f3cb9a02340b40e
ashdeep <ashdeepupadhyay@gmail.com>
2019-06-22 21:53:34 +0530

    making product api's more meaningfull

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,11 +5,26 @@ const mongoose=require('mongoose');
 const Product = require('../models/product')
 router.get('/',(req,res,next)=>{
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs=>{
+            const response = {
+                count : docs.length,
+                products : docs.map(doc=>{
+                    return{
+                        name:doc.name,
+                        price:doc.price,
+                        _id:doc._id,
+                        request :{
+                            type : 'GET',
+                            url : 'http://localhost:3000/products/'+doc._id
+                        }
+                    }
+                })
+            };
             console.log(docs);
           //  if(docs.length>=0)   {
-                res.status(200).json(docs);
+                res.status(200).json(response);
           //  }else{
           //      res.status(404).json({
           //          message:'no entries found'
@@ -35,32 +50,46 @@ router.post('/',(req,res,next)=>{
         _id : new mongoose.Types.ObjectId(),
         name : req.body.name,
         price : req.body.price
-    })
+    });
     product
         .save()
         .then(result=>{
             console.log(result);
             res.status(201).json({
-                message : 'Handling POST requests to /Products',
-                createdProduct: result
+                message : 'Created Product Successfully',
+                createdProduct: {
+                    name:result.name,
+                        price:result.price,
+                        _id:result._id,
+                        request :{
+                            type : 'GET',
+                            url : 'http://localhost:3000/products/'+result._id
+                        }
+                }
+            });
         })
         .catch(err=>{console.log(err);
                 res.status(500).json({
                     error: err
                 });
             });
-    
     });
-});
 
 router.get('/:productID',(req,res,next)=>{
     const id=req.params.productID;
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc=>{
             console.log("From database"+doc);
             if(doc){
-                res.status(200).json(doc);
+                res.status(200).json({
+                    product:doc,
+                    request:{
+                        type:'GET',
+                        url:'http://localhost:3000/products'
+                    }
+                });
             }else{
                 res.status(404).json({message:'NO Valid entry found for provided ID'});
             }
@@ -92,7 +121,13 @@ router.patch('/:productID',(req,res,next)=>{
     .exec()
     .then(result=>{
         console.log(result);
-        res.status(200).json(result);
+        res.status(200).json({
+            message:'Product Updated',
+            request :{
+                type : 'GET',
+                url : 'http://localhost:3000/products/'+id
+            }
+        });
     })
     .catch(err=>{
         console.log(err);
@@ -113,7 +148,14 @@ router.delete('/:productID',(req,res,next)=>{
         .exec()
         .then(result=>{
             console.log("deleted");
-            res.status(200).json(result);
+            res.status(200).json({
+                message:'Product deleted',
+                request:{
+                    type:'POST',
+                    url:'http://localhost:3000/products/',
+                    body:{name:'String',price:'Number'}
+                }
+            });
         })
         .catch(err=>{
             console.log(err);
