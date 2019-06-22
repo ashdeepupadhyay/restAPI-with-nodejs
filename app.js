0c4012398b07976235e6cf4bdae052b9efff6341
ashdeep <ashdeepupadhyay@gmail.com>
2019-06-22 14:23:54 +0530

    adding the logging middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
 //Routes which should handle request
 app.use('/products',productRoutes);
 app.use('/orders',ordersRoutes);
