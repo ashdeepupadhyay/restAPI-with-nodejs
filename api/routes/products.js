c1960470783654df3288f31e3aca287e18608dad
ashdeep <ashdeepupadhyay@gmail.com>
2019-06-23 18:37:12 +0530

    modify the filenames

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,7 +2,17 @@ const express=require('express');
 const router=express.Router();
 const mongoose=require('mongoose');
 const multer = require('multer');
-const upload = multer({dest : 'uploads/'});
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'./uploads/')
+    },
+    filename: function(req,file,cb){
+        cb(null,Date.now() + file.originalname);
+    }
+});
+
+const upload = multer({storage:storage});
 
 const Product = require('../models/product')
 router.get('/',(req,res,next)=>{
