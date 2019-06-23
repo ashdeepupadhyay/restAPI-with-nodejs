5063e7a69229e9a4ab09cd2726a57f4f6efc221f
ashdeep <ashdeepupadhyay@gmail.com>
2019-06-23 14:29:29 +0530

    making a post request and saving values in database

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,8 @@
 const express=require('express');
 const router=express.Router();
+const mongoose=require('mongoose');
+
+const Order=require('../models/order');
 
 router.get('/',(req,res,next)=>{
     res.status(200).json({
@@ -8,14 +11,23 @@ router.get('/',(req,res,next)=>{
 });
 
 router.post('/',(req,res,next)=>{
-    const order={
-        productID:req.body.productID,
-        quantity: req.body.quantity
-    }
-    res.status(201).json({
-        message:'order were created',
-        order : order
+    const order=new Order({
+        _id : mongoose.Types.ObjectId(),
+        productId : req.body.productId,
+        quantity : req.body.quantity
     });
+    order
+        .save()
+        .then(result=>{
+            console.log(result);
+            res.status(201).json(result);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
 });
 
 router.get('/:orderID',(req,res,next)=>{
